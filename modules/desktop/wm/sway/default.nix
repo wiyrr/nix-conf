bca66fe0c3a318738adfe78f404481c256ff7ab9
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2026-02-17 02:56:59 +0900

    sway:

--- modules/desktop/wm/sway/default.nix
+++ modules/desktop/wm/sway/default.nix
@@ -17,20 +17,21 @@ in
     programs.uwsm.waylandCompositors.sway = {
       prettyName = "Sway";
       comment = "";
-      # binPath = "/run/current-system/sw/bin/sway";
-      binPath = "${pkgs.sway}/bin/sway";
+      # binPath = "${pkgs.sway}/bin/sway";
+      binPath = "/run/current-system/sw/bin/sway";
     };
 
     programs.sway = {
       enable = true;
+      wrapperFeatures.gtk = true;
       xwayland.enable = false;
-      extraPackages = [ ];
+      extraPackages = lib.mkForce [ pkgs.autotiling-rs ];
     };
 
     hm' = {
-      #   wayland.windowManager.sway = {
-      #     enable = true;
-      #   };
+      wayland.windowManager.sway = {
+        enable = true;
+      };
       xdg.configFile."sway/config".source = ./dots/config;
     };
   };
