5dacc11b7a22bd85841c29c3a322c0299d1d7f61
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2026-02-20 06:15:25 +0900

    noctalia

--- modules/desktop/services/hypridle/default.nix
+++ modules/desktop/services/hypridle/default.nix
@@ -18,7 +18,8 @@ in
             before_sleep_cmd = "loginctl lock-session";
             after_sleep_cmd = "hyprctl dispatch dpms on";
             ignore_dbus_inhibit = false;
-            lock_cmd = "pidof hyprlock || hyprlock";
+            # lock_cmd = "pidof hyprlock || hyprlock";
+            lock_cmd = "noctalia-shell ipc call lockScreen lock";
           };
 
           listener = [
