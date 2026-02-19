599d121d52309e77b98dd2f53b9b4a3ae4b99f12
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2026-02-20 06:13:51 +0900

    yazi:

--- modules/programs/yazi/theme.nix
+++ modules/programs/yazi/theme.nix
@@ -1,4 +1,18 @@
 {
+  tabs = {
+    sep_inner.open = "";
+    sep_inner.close = "";
+    sep_outer.open = "";
+    sep_outer.close = "";
+  };
+
+  status = {
+    sep_left.open = "";
+    sep_left.close = "";
+    sep_right.open = "";
+    sep_right.close = "";
+  };
+
   icon = {
     prepend_dirs = [
       {
