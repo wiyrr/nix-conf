0aef47b8dffc5dcaf7b47fc6de32f1b69953e250
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2026-03-12 14:53:42 +0900

    feat(mango): update config

--- modules/desktop/programs/mango/default.nix
+++ modules/desktop/programs/mango/default.nix
@@ -3,13 +3,30 @@
   perSystem =
     { pkgs, ... }:
     let
+      inherit (self.cfg')
+        borderRadius
+        repeatRate
+        repeatDelay
+        cursorName
+        cursorSize
+        ;
+
       conf = pkgs.writeText "config.conf" ''
         ${builtins.readFile ./config.conf}
+
+        repeat_rate=${toString repeatRate}
+        repeat_delay=${toString repeatDelay}
+        # border_radius=${toString borderRadius}
+
+        cursor_theme=${cursorName}
+        cursor_size=${toString cursorSize}
+
       '';
     in
     {
       packages.mangowc = inputs.wrappers.lib.wrapPackage {
         inherit pkgs;
+        # package = inputs.mangowm.packages."${pkgs.stdenv.hostPlatform.system}".default;
         package = pkgs.mangowc.override { enableXWayland = false; };
         flags = {
           "-c" = "${conf}";
@@ -20,6 +37,7 @@
   flake.modules.nixos.mango =
     { pkgs, ... }:
     {
+      # imports = [ inputs.mangowm.nixosModules.mango ];
       programs.mangowc = {
         enable = true;
         package = self.packages.${pkgs.stdenv.hostPlatform.system}.mangowc;
