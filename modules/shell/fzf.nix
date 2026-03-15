ad3fba7390379eb8a7ff10e7228cdf4f4b4d067c
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2026-03-15 23:58:46 +0900

    fzf:

--- modules/shell/fzf.nix
+++ modules/shell/fzf.nix
@@ -1,23 +1,25 @@
 { inputs, self, ... }:
 {
-  perSystem =
-    { pkgs, ... }:
-    {
-      packages.fzf = inputs.wrappers.lib.wrapPackage {
-        inherit pkgs;
-        package = pkgs.fzf;
-        flags = {
-          "--border" = "sharp";
-          "--layout" = "reverse";
-          "--height" = "40%";
-          "--color" =
-            "bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8,fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC,marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8,selected-bg:#45475A,border:#6C7086,label:#CDD6F4";
-        };
-      };
-    };
+  # perSystem =
+  #   { pkgs, ... }:
+  #   {
+  #     packages.fzf = inputs.wrappers.lib.wrapPackage {
+  #       inherit pkgs;
+  #       package = pkgs.fzf;
+  #       flags = {
+  #         "--border" = "sharp";
+  #         "--layout" = "reverse";
+  #         "--height" = "40%";
+  #         "--color" =
+  #           "bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8,fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC,marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8,selected-bg:#45475A,border:#6C7086,label:#CDD6F4";
+  #       };
+  #     };
+  #   };
 
   flake.modules.nixos.shell = {
-    # environment.variables.FZF_DEFAULT_OPTS = "--layout=reverse --height=40% --border=sharp";
+    environment.variables = {
+      FZF_DEFAULT_OPTS = "--layout=reverse --height=40% --border=sharp --color base16";
+    };
     programs.fzf = {
       keybindings = true;
       fuzzyCompletion = true;
